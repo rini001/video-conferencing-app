69abd012cc9c453f193b8e7f9a97e0259c175df5
rini960 <debnathrenaissance95@gmail.com>
2022-06-14 11:49:08 +0530

    05

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -1,16 +1,26 @@
-import { useAVToggle } from '@100mslive/react-sdk';
+import { useAVToggle, useHMSActions } from '@100mslive/react-sdk';
 import React from 'react'
 
 export const Bottom = () => {
+    const hmsActions = useHMSActions();
     const {
         isLocalAudioEnabled,
         isLocalVideoEnabled,
         toggleAudio,
         toggleVideo
       } = useAVToggle();
+      const shareScreen = async ()=>{       
+try {
+    await hmsActions.setScreenShareEnabled(true);
+} catch (error) {
+    console.log("oops")
+}
+      }
   return (
     <div>
         <button onClick={toggleAudio}>{isLocalAudioEnabled ? "mute":"unmute"}</button>
+        <button onClick={toggleVideo}>{isLocalVideoEnabled ? "hide":"unhide"}</button>
+        <button onClick={shareScreen}>Share</button>
     </div>
   )
 }
